6b4ec8d2c573fa1310f0412fe2e3bdba1fe5a6e6
andres.navarrete <andres.navarrete@ibm.com>
2022-07-05 13:27:33 -0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'client';
-  url = '/server/api/readme';
+  url = '/api2/api/readme';
   // url = 'http://localhost:3000/api/readme';
 
   readme: any;
